43a7f61ad58a76a609cc533888a2e0141176d2de
Priscila Vergara <pavergaras@gmail.com>
2022-01-28 00:18:46 -0300

    se corrige archivo gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -9,7 +9,7 @@ def call(){
         withSonarQubeEnv('sonarqube') {
             sh "echo 'Calling sonar by ID!'"
             // Run Maven on a Unix agent to execute Sonar.
-            sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+            sh 'sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
         }
     }
 

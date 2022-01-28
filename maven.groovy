7e8871b13c6dc7b3f1f406eb212cb44df338c930
Priscila Vergara <pavergaras@gmail.com>
2022-01-27 23:05:09 -0300

    actualizo jenkins file para ejecución con parámetros y archivos groovy

---
+++ maven.groovy
@@ -0,0 +1,16 @@
+/*
+
+    forma de invocación de método call:
+
+    def ejecucion = load 'script.groovy'
+    ejecucion.call()
+
+*/
+
+def call(){
+  
+  stage(){
+    //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
+  }
+
+}
